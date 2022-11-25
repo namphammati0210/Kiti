ba9460298578de79fda353129c16364fa01680d8
Nam Pham <nam.pham@hats.solutions>
2022-11-25 20:54:51 +0700

    feat: 🎸 setup layouts based on taildwindcss

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,27 @@ import {
   // RouterProvider,
   // Route,
 } from "react-router-dom";
+
+// Import layouts
+import AdminLayout from "../layouts/Admin";
+
 import App from "../App";
 
 const routes = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <App />,
+  // },
   {
     path: "/",
-    element: <App />,
+    element: <AdminLayout />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+    ],
   },
 ]);
 
