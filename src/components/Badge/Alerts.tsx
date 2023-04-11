4d16ea60e02a6461e956b5ad7a00103a34519885
Nam Pham <nam.pham@hats.solutions>
2023-04-11 18:42:41 +0700

    Feat: close Alerts component

--- src/components/Badge/Alerts.tsx
+++ src/components/Badge/Alerts.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 interface IProps {
   variant: "error" | "success" | "info";
   message: string;
@@ -10,19 +11,25 @@ const variantColor = {
 };
 
 const Alerts = ({ variant, message }: IProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
   return (
-    <div
-      className={`text-white px-6 py-4 border-0 rounded relative mb-4 ${variantColor[variant]}`}
-    >
-      <span className="text-xl inline-block mr-5 align-middle">
-        <i className="fas fa-bell"></i>
-      </span>
-      <span className="inline-block align-middle mr-8">
-        <b className="capitalize">Error:</b> {message}
-      </span>
-      <button className="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none">
-        <span>×</span>
-      </button>
+    <div>
+      {isOpen && (
+        <div
+          className={`text-white px-6 py-4 border-0 rounded relative mb-4 ${variantColor[variant]}`}
+        >
+          <span className="text-xl inline-block mr-5 align-middle">
+            <i className="fas fa-bell"></i>
+          </span>
+          <span className="inline-block align-middle mr-8">
+            <b className="capitalize">{variant}:</b> {message}
+          </span>
+          <button className="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none">
+            <span onClick={() => setIsOpen(false)}>×</span>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
